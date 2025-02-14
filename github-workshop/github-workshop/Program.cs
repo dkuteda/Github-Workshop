87a68990a8577f0234305f8ede812d7fabc5b1bd
Teo_ptrs <teopeeters2006@gmail.com>
2025-02-14 12:55:38 +0100

    first commit

---
+++ github-workshop/github-workshop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_workshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
